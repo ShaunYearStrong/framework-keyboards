e79358a77cf11a3d15acf2f98ca20afd9126bd34
ShaunYearStrong <shaun@shaundillon.info>
2015-05-15 13:05:15 +0100

    Converting tabs to spaces

--- angular/src/scripts/controller.js
+++ angular/src/scripts/controller.js
@@ -5,56 +5,56 @@
 (function() {
     'use strict';
 
-	app.controller('KeyboardController', KeyboardController);
+    app.controller('KeyboardController', KeyboardController);
 
-	KeyboardController.$inject = ['$scope'];
+    KeyboardController.$inject = ['$scope'];
 
-	function KeyboardController($scope) {
-		$scope.typed = "";
-		$scope.state = {
-			capsLock: false,
-			shift: false
-		};
+    function KeyboardController($scope) {
+        $scope.typed = "";
+        $scope.state = {
+            capsLock: false,
+            shift: false
+        };
 
-		$scope.handleType = handleType;
-		$scope.capsLock = capsLock;
-		$scope.shiftKey = shiftKey;
-		$scope.deleteKey = deleteKey;
+        $scope.handleType = handleType;
+        $scope.capsLock = capsLock;
+        $scope.shiftKey = shiftKey;
+        $scope.deleteKey = deleteKey;
 
-		function handleType($event) {
-			if($event.target.attributes['data-value']) {
-				$scope.typed += $event.target.attributes['data-value'].value;
-			}
-		}
+        function handleType($event) {
+            if($event.target.attributes['data-value']) {
+                $scope.typed += $event.target.attributes['data-value'].value;
+            }
+        }
 
-		function capsLock($event) {
-			$scope.state.shift = false;
+        function capsLock($event) {
+            $scope.state.shift = false;
 
-			if ($scope.state.capsLock) {
-				$scope.state.capsLock = false;
-			} else {
-				$scope.state.capsLock = true;
-			}
+            if ($scope.state.capsLock) {
+                $scope.state.capsLock = false;
+            } else {
+                $scope.state.capsLock = true;
+            }
 
-			$event.stopPropagation();
-		}
+            $event.stopPropagation();
+        }
 
-		function shiftKey($event) {
-			$scope.state.capsLock = false;
+        function shiftKey($event) {
+            $scope.state.capsLock = false;
 
-			if ($scope.state.shift) {
-				$scope.state.shift = false;
-			} else {
-				$scope.state.shift = true;
-			}
+            if ($scope.state.shift) {
+                $scope.state.shift = false;
+            } else {
+                $scope.state.shift = true;
+            }
 
-			$event.stopPropagation();
-		}
+            $event.stopPropagation();
+        }
 
-		function deleteKey($event) {
-			$scope.typed  = $scope.typed.substring(0, $scope.typed.length - 1);
-			$event.stopPropagation();
-		}
-	}
+        function deleteKey($event) {
+            $scope.typed  = $scope.typed.substring(0, $scope.typed.length - 1);
+            $event.stopPropagation();
+        }
+    }
 
 })();

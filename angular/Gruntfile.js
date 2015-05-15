84d78d47bb845b25d3fba48496597a6797ec161a
ShaunYearStrong <shaun@shaundillon.info>
2015-05-15 13:04:40 +0100

    whitespace in gruntfile

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -1,69 +1,69 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/scripts/*.js',
-        dest: 'build/scripts/app.min.js'
-      }
-    },
-    sass: {
-        options: {
-            sourceMap: true
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/scripts/*.js',
+                dest: 'build/scripts/app.min.js'
+            }
         },
-        dist: {
-            files: {
-                'build/css/main.css': 'src/styles/main.scss'
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'build/css/main.css': 'src/styles/main.scss'
+                }
+            }
+        },
+        jscs: {
+            src: "src/scripts/*.js",
+            options: {
+                config: ".jscsrc",
+                requireCurlyBraces: [ "if" ]
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/scripts/*.js']
+        },
+        csslint: {
+            analysis: {
+                src: ['build/css/main.css']
+            }
+        },
+        watch: {
+            css: {
+                files: 'src/styles/**/*.scss',
+                tasks: ['sass', 'csslint'],
+                options: {
+                    livereload: true
+                },
+            },
+            js: {
+                files: 'src/scripts/*.js',
+                tasks: ['uglify', 'jscs', 'jshint'],
+                options: {
+                    livereload: true
+                }
             }
         }
-    },
-    jscs: {
-      src: "src/scripts/*.js",
-      options: {
-          config: ".jscsrc",
-          requireCurlyBraces: [ "if" ]
-      }
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/scripts/*.js']
-    },
-    csslint: {
-      analysis: {
-        src: ['build/css/main.css']
-      }
-    },
-    watch: {
-      css: {
-        files: 'src/styles/**/*.scss',
-        tasks: ['sass', 'csslint'],
-        options: {
-          livereload: true
-        },
-      },
-      js: {
-        files: 'src/scripts/*.js',
-        tasks: ['uglify', 'jscs', 'jshint'],
-        options: {
-          livereload: true
-        }
-      }
-    }
-  });
+    });
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks("grunt-jscs");
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks("grunt-jscs");
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Start it on up
-  grunt.registerTask('default', ['uglify', 'sass', 'jscs', 'jshint', 'csslint', 'watch']);
+    // Start it on up
+    grunt.registerTask('default', ['uglify', 'sass', 'jscs', 'jshint', 'csslint', 'watch']);
 
 };
